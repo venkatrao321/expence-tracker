01ba4d36f72e61b05dfac3e9b478dd802e2de789
venkatrao321 <venkatrao321@gmail.com>
2024-04-03 19:41:14 +0530

    added model for wallet

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,25 @@
-const Model = () => {
+import ReactModal from 'react-modal';
+
+const Model = ({type,openBalModel,SetOpenBalModel }) => {
     return (  
         <>
+         <ReactModal className={'walletModel position-absolute top-50 start-50 translate-middle'} isOpen={openBalModel} onRequestClose={()=>{
+            SetOpenBalModel(false)
+          }}
+          ariaHideApp={false}>
+         {type==='walletModal'?  (<div className="d-flex gap-2">
+                <input type="number" className="form-control" placeholder="Income Amount"></input>
+                <button className="btn btn-warning">AddBalance</button>
+                <button type="button" class="btn btn-secondary"onClick={()=>{
+            SetOpenBalModel(false)
+          }}>Cancel</button>
+            </div>):
             
+            ''
+            
+            
+            }
+            </ReactModal>
         </>
     );
 }
--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import Card from "../ui/Cards";
 import './wallet.css'
-import ReactModal from 'react-modal';
+import Model from "../Modal/Modal";
 function Wallet({walletBalance}) {
   const [openBalModel,SetOpenBalModel]=useState(false);
  
     return ( 
         <>
-          <Card buttonText={'+ Add Income'} cardText={'Wallet Balance:'} btncolor={'green'} SetOpenBalModel={SetOpenBalModel}  walletMoney={walletBalance}></Card>  
-          <ReactModal className={'walletModel'} isOpen={openBalModel} onRequestClose={()=>{
-            SetOpenBalModel(false)
-          }}
-          ariaHideApp={false}>
-
-          <p>Modal text!</p>
+          <Card buttonText={'+ Add Income'} cardText={'Wallet Balance:'} btncolor={'green'} SetOpenBalModel={SetOpenBalModel}  walletMoney={walletBalance}></Card>
+         <Model type="walletModal"openBalModel={openBalModel} SetOpenBalModel={SetOpenBalModel}></Model>
           
-          </ReactModal>
         </>
     
      );
